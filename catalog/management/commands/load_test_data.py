609d215b21865715329920878b35701debeed265
aleksey <alesksey.p.soft@gmail.com>
2025-01-09 12:45:56 +0300

    Добавлена вставка остальных тестовых данных

--- catalog/management/commands/load_test_data.py
+++ catalog/management/commands/load_test_data.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
-from catalog.models import Employee
+from catalog.models import Employee, Department, DepartmentMembers, Team, DepartmentTeams
 from faker import Faker
 
 class Command(BaseCommand):
@@ -7,11 +7,26 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         fake = Faker()
-        e = Employee(first_name=fake.name(),
+        employee = Employee(first_name=fake.name(),
                      last_name=fake.last_name(),
                      patronymic_name=fake.name(),
                      salary=50)
-        e.save(force_insert=True)
+        employee.save()
+        department = Department(
+            name=fake.name(),
+            department_head=employee)
+        department.save()
+        department_members = DepartmentMembers(
+            name=fake.name(),
+            employee=employee,
+            department=department)
+        department_members.save()
+        team = Team(
+            name=fake.name(),
+            team_leader=employee)
+        team.save()
+        department_team = DepartmentTeams(team=team, department=department)
+        department_team.save()
 
         self.stdout.write(
             self.style.SUCCESS('Данные успешно вставлены')
